c443551c01df548759a82dbb278c6ed05e69aaad
Marcos Paulo Belasco de Almeida <patu.duck@gmail.com>
2018-07-22 01:54:34 -0300

    Create login component

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { SharedModule } from './../shared/shared.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   imports: [
     SharedModule,
     LoginRoutingModule
   ],
-  declarations: []
+  declarations: [LoginComponent],
+  exports: [LoginComponent]
 })
 export class LoginModule { }
