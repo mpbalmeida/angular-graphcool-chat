f1726adc41c7e27fc002f5727486e62ba437ce25
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-25 20:59:41 -0200

    Add header component

---
+++ src/app/dashboard/components/dashboard-header/dashboard-header.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../../core/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard-header',
+  templateUrl: './dashboard-header.component.html',
+  styleUrls: ['./dashboard-header.component.scss']
+})
+export class DashboardHeaderComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import {SharedModule} from '../shared/shared.module';
 import { DashboardHomeComponent } from './components/dashboard-home/dashboard-home.component';
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 
 @NgModule({
   imports: [
     SharedModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardHomeComponent]
+  declarations: [DashboardHomeComponent, DashboardHeaderComponent]
 })
 export class DashboardModule { }
