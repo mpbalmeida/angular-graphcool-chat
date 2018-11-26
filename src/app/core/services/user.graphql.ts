f1ee3738d28f6d54e6aee05d4e3983ce328112ad
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-26 17:18:03 -0200

    Add all users query

---
+++ src/app/core/services/user.graphql.ts
@@ -0,0 +1,18 @@
+import {User} from '../models/user.model';
+import gql from 'graphql-tag';
+
+export interface AllUsersQuery {
+  allUsers: User[];
+}
+
+export const ALL_USERS_QUERY = gql`
+  query AllUsersQuery {
+    allUsers(
+      orderBy: name_ASC
+    ) {
+      id
+      name
+      email
+    }
+  }
+`;
