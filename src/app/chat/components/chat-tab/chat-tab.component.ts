e1f3625a123a7c55e362e32be3951088258da184
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-26 16:38:32 -0200

    Add router for chat and users

--- src/app/chat/components/chat-tab/chat-tab.component.ts
+++ src/app/chat/components/chat-tab/chat-tab.component.ts
@@ -4,9 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-chat-tab',
   template: `
     <nav mat-tab-nav-bar backgroundColor="primary">
-      <a mat-tab-link>Chats</a>
-      <a mat-tab-link>Users</a>
+      <a mat-tab-link
+        routerLink="./"
+        routerLinkActive
+        #chatRla="routerLinkActive"
+        [active]="chatRla.isActive"
+        [routerLinkActiveOptions]="{exact: true}">
+        Chats
+      </a>
+      <a mat-tab-link
+        routerLink="users"
+        routerLinkActive
+        #usersRla="routerLinkActive"
+        [active]="usersRla.isActive">
+        Users
+      </a>
     </nav>
+    <router-outlet></router-outlet>
   `,
   styles: []
 })
