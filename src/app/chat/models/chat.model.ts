6b5f934c979e76d58c7f54e1226b8cd2eb57cac0
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-27 00:33:56 -0200

    Create chat and message models

---
+++ src/app/chat/models/chat.model.ts
@@ -0,0 +1,12 @@
+import {User} from '../../core/models/user.model';
+import {Message} from './message.model';
+
+export class Chat {
+
+  id?: string;
+  createdAt?: string;
+  isGroup?: boolean;
+  title?: string;
+  users?: User[];
+  messages?: Message[];
+}
---
+++ src/app/chat/services/chat.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import {ChatModule} from '../chat.module';
+
+@Injectable({
+  providedIn: ChatModule
+})
+export class ChatService {
+
+  constructor() { }
+}
