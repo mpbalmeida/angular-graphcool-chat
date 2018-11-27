82ca5e3db714ce85c2ff0a3dea397352aa1f2e8b
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-27 00:55:28 -0200

    Create query to search user chats

--- src/app/chat/services/chat.service.ts
+++ src/app/chat/services/chat.service.ts
@@ -1,10 +1,30 @@
 import { Injectable } from '@angular/core';
 import {ChatModule} from '../chat.module';
+import {Observable, pipe} from 'rxjs';
+import {Chat} from '../models/chat.model';
+import {Apollo} from 'apollo-angular';
+import {AuthService} from '../../core/services/auth.service';
+import {AllChatsQuery, USER_CHATS_QUERY} from './chat.graphl';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: ChatModule
 })
 export class ChatService {
 
-  constructor() { }
+  constructor(
+    private apollo: Apollo,
+    private authService: AuthService
+  ) { }
+
+  getUserChats(): Observable<Chat[]> {
+    return this.apollo.query<AllChatsQuery>({
+      query: USER_CHATS_QUERY,
+      variables: {
+        userId: this.authService.authUser.id
+      }
+    }).pipe(
+      map(res => res.data.allChats)
+    );
+  }
 }
