22faf4186cd2624f22c836ace7efb3182f5d7d96
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-28 13:57:28 -0200

    Capture chat in chat window component

--- src/app/chat/components/chat-window/chat-window.component.ts
+++ src/app/chat/components/chat-window/chat-window.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {Chat} from '../../models/chat.model';
+import {Subscription} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat-window',
   templateUrl: './chat-window.component.html',
   styleUrls: ['./chat-window.component.scss']
 })
-export class ChatWindowComponent implements OnInit {
+export class ChatWindowComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  chat: Chat;
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.subscriptions.push(
+      this.route.data
+        .pipe(
+          map(routeData => this.chat = routeData.chat)
+        )
+        .subscribe()
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach(s => s.unsubscribe());
   }
 
 }
