a9d36158e8efe813526074343d304cbfc283773d
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-28 14:39:04 -0200

    Add Query to find user by id

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import {Apollo} from 'apollo-angular';
 import {Observable} from 'rxjs';
 import {User} from '../models/user.model';
-import {ALL_USERS_QUERY, AllUsersQuery} from './user.graphql';
+import {ALL_USERS_QUERY, AllUsersQuery, GET_USER_BY_ID_QUERY} from './user.graphql';
 import {map} from 'rxjs/operators';
 
 @Injectable({
@@ -24,5 +24,16 @@ export class UserService {
       }).pipe(
         map(res => res.data.allUsers)
       );
+  }
+
+  getUserById(id: string): Observable<User> {
+    return this.apollo.query({
+      query: GET_USER_BY_ID_QUERY,
+      variables: {
+        userId: id
+      }
+    }).pipe(
+      map(res => res.data.User)
+    );
   }
 }
