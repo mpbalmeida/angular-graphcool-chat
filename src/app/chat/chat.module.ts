49456aafe56732eec30626000498dba0f0f84772
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-29 19:55:18 -0200

    Add chat message template

--- src/app/chat/chat.module.ts
+++ src/app/chat/chat.module.ts
@@ -6,12 +6,13 @@ import { ChatTabComponent } from './components/chat-tab/chat-tab.component';
 import { ChatUsersComponent } from './components/chat-users/chat-users.component';
 import { ChatListComponent } from './components/chat-list/chat-list.component';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ChatMessageComponent } from './components/chat-message/chat-message.component';
 
 @NgModule({
   imports: [
     SharedModule,
     ChatRoutingModule
   ],
-  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent, ChatWindowComponent]
+  declarations: [ChatTabComponent, ChatUsersComponent, ChatListComponent, ChatWindowComponent, ChatMessageComponent]
 })
 export class ChatModule { }
