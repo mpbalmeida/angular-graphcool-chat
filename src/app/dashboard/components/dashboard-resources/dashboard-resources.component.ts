5c9ed853fffdb9eb339683d9f68fb08e3e5367d9
Marcos Almeida <marcos.almeida@abacomm.com.br>
2018-11-29 23:36:29 -0200

    Add link to user route

--- src/app/dashboard/components/dashboard-resources/dashboard-resources.component.ts
+++ src/app/dashboard/components/dashboard-resources/dashboard-resources.component.ts
@@ -15,6 +15,11 @@ export class DashboardResourcesComponent implements OnInit {
       url: '/dashboard/chat',
       icon: 'chat_bubble',
       title: 'My Chats'
+    },
+    {
+      url: '/dashboard/chat/users',
+      icon: 'people',
+      title: 'All users'
     }
   ];
 
